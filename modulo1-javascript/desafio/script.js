b6edecd9cf8a597a88c483d933ac040c2c6858bd
mayconb2 <mayconb2@gmail.com>
2020-05-24 22:54:38 -0300

    WIP - Busca funcionando conforme solicitado no desafio - Falta implementar um design melhor, separando as pessoas do resumo.

--- modulo1-javascript/desafio/script.js
+++ modulo1-javascript/desafio/script.js
@@ -1,7 +1,8 @@
 const form = document.querySelector('#frm');
 const inputText = document.querySelector('#input-text');
 const searchButton = document.querySelector('#search-button');
-let filteredPerson = document.querySelector('#filtered-person');
+const filteredPerson = document.querySelector('#filtered-person');
+const personPummary = document.querySelector('#person-summary');
 
 let globalAllPerson = [];
 
@@ -13,26 +14,6 @@ window.addEventListener ('load' , () => {
 
     fetchFromAPI();
 });
-
-function search(textValue) {
-    
-    renderPerson(textValue);
-}
-
-inputText.addEventListener('keyup', (event) => {
-
-    if (event.target.value == '') {
-        searchButton.disabled = true;
-    }
-
-    if (event.target.value != '') {
-        searchButton.disabled = false;
-    }
-
-    if (event.key == 'Enter' && event.target.value != '') {
-        search();
-    };
-})
 
 async function fetchFromAPI() {
 
@@ -52,10 +33,50 @@ async function fetchFromAPI() {
     });
 }
 
+searchButton.addEventListener('click', () => {
+
+    let nameToSearch = inputText.value.toLowerCase();
+
+    if (inputText.value == '' || inputText.value == null) {
+        return;
+    }
+    //to do = DRY
+    search(nameToSearch)
+
+});
+
+inputText.addEventListener('keyup', (event) => {
+    let nameToSearch = inputText.value.toLowerCase();
+
+    if (event.target.value == '') {
+        searchButton.disabled = true;
+    }
+
+    if (event.target.value != '') {
+        searchButton.disabled = false;
+    }
+
+    if (event.key == 'Enter' && event.target.value != '') {
+        //to do = DRY
+        search(nameToSearch);
+    };
+})
+
+function search(textValue) {
+    renderPerson(textValue);
+    renderSummary(textValue);
+}
+
 function renderPerson(textValue) {
+
     let personHTML = '';
-    
-    globalAllPerson.map(person => {
+
+    let filteredFromAll = filterArray(textValue).sort((a,b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    //render filtered array in html
+    filteredFromAll.map(person => {
         const {name, gender, age, picture} = person;
         
         let peopleHTML = `
@@ -79,8 +100,60 @@ function renderPerson(textValue) {
     filteredPerson.innerHTML = personHTML;
 }
 
-function renderSummary() {
+function renderSummary(textValue) {
+    let summaryHTML = '';
 
+    let filteredFromAll = filterArray(textValue);
+
+    const sumMale = filteredFromAll.reduce((acc,cur) => {
+        if (cur.gender == 'male') {
+            acc += 1;
+        }
+        return acc;
+    },0);
+
+    const sumFemale = filteredFromAll.reduce((acc,cur) => {
+        if (cur.gender == 'female') {
+            acc += 1;
+        }
+        return acc;
+    },0);
+
+    const sumAges = filteredFromAll.reduce((acc,cur) => {
+        return acc + cur.age;
+    },0);
+
+    const averageAges = (sumAges / filteredFromAll.length).toFixed(2);
+
+    personPummary.innerHTML = `
+
+        <div>
+            Gender Male: <b>${sumMale}</b>
+        <div>
+
+        <div>
+            Gender Female: <b>${sumFemale}</b>
+        <div>
+
+        <div>
+            Sum Ages: <b>${sumAges}</b>
+        <div>
+
+        <div>
+            Average Ages: <b>${averageAges}</b>
+        <div>
+
+    `;
+}
+
+function filterArray(text) {
+    let filteredFromAll = globalAllPerson.filter(people => {
+        return people.name
+            .toLowerCase()
+            .includes(text)
+    });
+
+    return filteredFromAll;
 }
 
 function clearResults() {
