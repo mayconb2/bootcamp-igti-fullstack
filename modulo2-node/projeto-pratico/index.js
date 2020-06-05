be00d1ccac39a6b3d7c7670e2ebc87da24d8b3e8
mayconb2 <mayconb2@gmail.com>
2020-06-05 00:08:04 -0300

    Criando arquivos separados

--- modulo2-node/projeto-pratico/index.js
+++ modulo2-node/projeto-pratico/index.js
@@ -11,11 +11,7 @@ async function start() {
 
     // createFile();
 
-    // sumCities('AC');
-
-    // sumCitiesAllStates();
-
-    writeCitisFile();
+    sumCities('AC');
 
     
 
@@ -40,61 +36,6 @@ async function createFile() {
         console.error('Erro: ', err)
     }
 
-}
-
-async function writeCitisFile () {
-
-    const modifyStatesstatesAndCities = [];
-
-    const rowState = await fs.readFile(estadosJson, 'utf8');
-    const states = JSON.parse(rowState);
-    
-    const rowCities = await fs.readFile(cidadesJson, 'utf8');
-    const cities = await JSON.parse(rowCities);
-    
-    const modifyStates = states.map(state => {
-        const {ID, Sigla, Nome} = state;
-        return {
-            idState : ID,
-            Sigla: Sigla,
-            stateName: Nome,
-            cities : {}
-        }
-        
-    });
-
-    modifyStates.map(state => {
-        modifyStatesstatesAndCities.push(state);
-    });
-
-    const modifyCities = cities.map(city => {
-        const {ID, Nome, Estado} = city;
-        return {
-            idCity: ID,
-            cityName: Nome,
-            idState: Estado
-        }
-    });
-
-    modifyCities.map(city => {
-        
-       if (city.idCity === '100') {
-           const indexEstate = modifyStatesstatesAndCities.findIndex(state => {
-                return state.idState === city.idState;
-           });
-
-           modifyStatesstatesAndCities[indexEstate].cities.push('a')
-           console.log(modifyStatesstatesAndCities[indexEstate].cities)
-
-        //    modifyStatesstatesAndCities[indexEstate].push(city)
-       }
-
-        // modifyStatesstatesAndCities[indexState].cities.push(city);
-
-
-    });
-
-    // console.log(modifyStatesstatesAndCities)
 }
 
 
@@ -123,20 +64,6 @@ async function writeCities(idState) {
 
 }
 
-async function logSumCities(stateName) {
-    
-    const rowState = await fs.readFile(`${stateName}.json`, 'utf8');
-    const state = JSON.parse(rowState);
-
-    let sum = 0;
-
-    state.map(city => {
-        sum += 1;
-    });
-    console.log(`A soma das cidades do estado ${stateName} é ${sum}`);
-    // return sum;
-}
-
 async function sumCities(stateName) {
     
     const rowState = await fs.readFile(`${stateName}.json`, 'utf8');
@@ -147,38 +74,10 @@ async function sumCities(stateName) {
     state.map(city => {
         sum += 1;
     });
-    // console.log(`A soma das cidades do estado ${stateName} é ${sum}`);
-    return sum;
+
+    console.log(`A soma das cidades do estado ${stateName} é ${sum}`);
 }
 
-// async function sumCitiesAllStates() {
-
-//     const allStates = [];
-//     const sumCitiesFromStates = [];
-
-//     const rowAllStates = await fs.readFile(estadosJson, 'utf8');
-//     const states = await JSON.parse(rowAllStates);
-
-//     states.map(state => {
-//         allStates.push(state.Sigla);
-//     })
-
-//     // console.log(allStates)
-    
-//      allStates.map(state => {
-//       sumCitiesFromStates.push(await sumCities(state));
-      
-//     });
-    
-//     console.log(sumCitiesFromStates)
-    
-
-//     // console.log(allStates[0])
-
-//     // let soma = await sumCities(allStates[0])
-
-//     // console.log(soma)
-// }
 
 
 start();
